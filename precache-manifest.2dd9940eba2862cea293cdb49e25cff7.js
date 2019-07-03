9d59824aede4c181180481ad28e214bdf2c06a92
Katerina Zenchik <e.zenchik@gmail.com>
2019-07-03 19:24:22 +0300

    add gh-pages

---
+++ precache-manifest.2dd9940eba2862cea293cdb49e25cff7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bd22da765294eaf549a6420e7d225e",
+    "url": "/piskel-clone-landing/index.html"
+  },
+  {
+    "revision": "4dd1bbfdb53c4c6536ae",
+    "url": "/piskel-clone-landing/static/css/main.b50b9ab5.chunk.css"
+  },
+  {
+    "revision": "a9633e3a9f8b0a7beecd",
+    "url": "/piskel-clone-landing/static/js/2.3e0568ea.chunk.js"
+  },
+  {
+    "revision": "4dd1bbfdb53c4c6536ae",
+    "url": "/piskel-clone-landing/static/js/main.b17c06c9.chunk.js"
+  },
+  {
+    "revision": "25605de0e8d47daa533a",
+    "url": "/piskel-clone-landing/static/js/runtime~main.7ae70a90.js"
+  },
+  {
+    "revision": "fc6b47f269d7d73cca95d31f4d3b47cb",
+    "url": "/piskel-clone-landing/static/media/Panda.fc6b47f2.gif"
+  },
+  {
+    "revision": "7fafb7f2196c4aaa137efe60aec728ef",
+    "url": "/piskel-clone-landing/static/media/colors.7fafb7f2.gif"
+  },
+  {
+    "revision": "6dbb48b3b021087e43cb931fdb994153",
+    "url": "/piskel-clone-landing/static/media/copyright.6dbb48b3.svg"
+  },
+  {
+    "revision": "c770fb96bf3ac7b064ce2a36bb1fcd96",
+    "url": "/piskel-clone-landing/static/media/llama.c770fb96.gif"
+  },
+  {
+    "revision": "434d6969832324a797bf8e7abe5d83e6",
+    "url": "/piskel-clone-landing/static/media/piskel-screenshot.434d6969.png"
+  }
+]);
